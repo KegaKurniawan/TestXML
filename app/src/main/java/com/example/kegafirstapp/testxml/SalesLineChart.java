2a3c3233cf5326c98553df39847a5e3a97ace644
KegaKurniawan <chaos3920@gmail.com>
2016-09-09 14:35:09 +0700

    Line chart sales sekali retrieve

--- app/src/main/java/com/example/kegafirstapp/testxml/SalesLineChart.java
+++ app/src/main/java/com/example/kegafirstapp/testxml/SalesLineChart.java
@@ -30,6 +30,10 @@ public class SalesLineChart extends ActionBarActivity {
     PreparedStatement stmt;
     ResultSet rs;
     String query;
+    Integer counter,sp;
+
+    ArrayList<Integer> sales_value;
+    ArrayList<String> month_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +54,7 @@ public class SalesLineChart extends ActionBarActivity {
         spinYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                retrieveSql();
                 getValue();
                 spinQuarter.setSelection(0);
             }
@@ -63,7 +68,7 @@ public class SalesLineChart extends ActionBarActivity {
         spinQuarter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+                getValue();
             }
 
             @Override
@@ -73,15 +78,49 @@ public class SalesLineChart extends ActionBarActivity {
         });
     }
 
-    private void getValue(){
+    private void retrieveSql(){
+        sales_value = new ArrayList<>();
+        month_list = new ArrayList<>();
         String year = spinYear.getSelectedItem().toString();
         connectionClass3 = new ConnectionClass3();
-        LineChart chart = (LineChart) findViewById(R.id.LineChart);
         query = "select b.Bulan, isnull(c.jumlah, 0) as jumlah from [Demo Database NAV (9-0)].[dbo].[Android_test_View_1] b LEFT JOIN\n" +
                 "(select substring(convert(varchar,a.[Posting Date],126),6, 2) as bulan, sum (a.[Sales (LCY)]) as jumlah \n" +
                 "from [Demo Database NAV (9-0)].[dbo].[CRONUS International Ltd_$Cust_ Ledger Entry] a where year(a.[Posting Date]) = '"+year+"' " +
                 "group by substring(convert(varchar,[Posting Date],126),6, 2)) c  \n" +
                 "ON c.bulan = b.no order by b.no";
+        try{
+
+            Connection con = connectionClass3.CONN();
+            stmt = con.prepareStatement(query);
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                int id = rs.getInt("jumlah");
+                sales_value.add(id);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            Connection con = connectionClass3.CONN();
+            stmt = con.prepareStatement(query);
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                String id = rs.getString("Bulan");
+                String sub_id = id.substring(0, 3);
+                month_list.add(sub_id);
+                //BUAT NYARI BUG INI
+                //System.out.println(xAxis);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void getValue(){
+        LineChart chart = (LineChart) findViewById(R.id.LineChart);
 
         LineData data = new LineData(getXAxisValues(), getDataSet());
         chart.setData(data);
@@ -93,28 +132,21 @@ public class SalesLineChart extends ActionBarActivity {
     private ArrayList<ILineDataSet> getDataSet() {
         ArrayList<ILineDataSet> dataSets = null;
         ArrayList<Entry> valueSet1 = new ArrayList<>();
-        try{
 
-            Connection con = connectionClass3.CONN();
-            stmt = con.prepareStatement(query);
-            rs = stmt.executeQuery();
-            int counter = 0;
-            while (rs.next()){
-                int id = rs.getInt("jumlah");
-                //Float fid = new Float(id);
-                Entry v1 = new Entry(id, counter);
-                valueSet1.add(v1);
-                //BUAT NYARI BUG INI
-                System.out.println(v1);
-                //System.out.println(counter);
-                counter++;
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+        getchoose();
+
+        int count2 = 0;
+        while(counter != 0){
+            int fill_chart = sales_value.get(sp);
+            Entry v1 = new Entry(fill_chart, count2);
+            valueSet1.add(v1);
+            //BUAT NYARI BUG INI
+            System.out.println(v1);
+            //System.out.println(counter);
+            counter--;
+            count2++;
+            sp++;
         }
-
-        System.out.println("Ini isi array yang ketiga :"+valueSet1.get(11));
-
 
         LineDataSet lineDataSet1 = new LineDataSet(valueSet1, "Sales");
         lineDataSet1.setColor(Color.rgb(155, 0, 0));
@@ -126,20 +158,42 @@ public class SalesLineChart extends ActionBarActivity {
 
     private ArrayList<String> getXAxisValues() {
         ArrayList<String> xAxis = new ArrayList<>();
-        try{
-            Connection con = connectionClass3.CONN();
-            stmt = con.prepareStatement(query);
-            rs = stmt.executeQuery();
-            while (rs.next()){
-                String id = rs.getString("Bulan");
-                String sub_id = id.substring(0, 3);
-                xAxis.add(sub_id);
-                //BUAT NYARI BUG INI
-                //System.out.println(xAxis);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
+
+        getchoose();
+
+        while(counter != 0){
+            String fill_month = month_list.get(sp);
+            xAxis.add(fill_month);
+            //BUAT NYARI BUG INI
+            //System.out.println(v1);
+            //System.out.println(counter);
+            counter--;
+            sp++;
         }
+
         return xAxis;
+    }
+
+    private void getchoose(){
+        String choose = spinQuarter.getSelectedItem().toString();
+
+        if(choose.equals("Quarter 1")){
+            counter = 3;
+            sp = 0;
+        }else if(choose.equals("Quarter 2")){
+            counter = 3;
+            sp = 3;
+        }else if(choose.equals("Quarter 3")){
+            counter = 3;
+            sp = 6;
+        }
+        else if(choose.equals("Quarter 4")){
+            counter = 3;
+            sp = 9;
+        }
+        else{
+            counter = 12;
+            sp = 0;
+        }
     }
 }
